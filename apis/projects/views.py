b2834aa8de4ee5e29512d32ab3fce1dd9f263a6c
chenzhida <chenzida@sidewalkgroup.com>
2021-12-17 18:08:28 +0800

    重写更新接口

--- apis/projects/views.py
+++ apis/projects/views.py
@@ -6,6 +6,8 @@ from utils.time_format import time_format
 from projects.utils import get_count_by_project
 from projects.serializer import *
 from interfaces.models import Interfaces
+
+
 # Create your views here.
 
 
@@ -60,3 +62,30 @@ class ProjectsViewSet(ModelViewSet):
         datas.append(serializer.data)
         datas_list = time_format(datas)
         return Response(datas_list)
+
+    # 重写部分更新接口
+    def partial_update(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        try:
+            project_obj = Projects.objects.get(is_delete=False, pk=pk)
+        except:
+            return Response(data={'pk': 'id值为空'}, status=400)
+        serializer = ProjectModelSerializer(instance=project_obj, data=request.data, partial=True,
+                                            context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        book_obj = serializer.save()
+        return Response(ProjectModelSerializer(book_obj).data)
+
+    # 重写更新接口
+    def update(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if pk:
+            try:
+                project_obj = Projects.objects.get(is_delete=False, pk=pk)
+            except:
+                return Response(data={'pk': 'id值为空'}, status=400)
+            serializer = ProjectModelSerializer(instance=project_obj, data=request.data, partial=False,
+                                                context={'request': request})
+            serializer.is_valid(raise_exception=True)
+            book_obj = serializer.save()
+            return Response(ProjectModelSerializer(book_obj).data)
